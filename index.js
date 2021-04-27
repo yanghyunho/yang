3fa2205e36270a37f09a2013ad1daff60fefab49
yhh1973 <yhh1973@gmail.com>
2021-04-27 13:18:38 +0900

    처음

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://yang:!mryang0928!@yang.ovzod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected.....'))
+.catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
