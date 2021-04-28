99bce6d5c52ca5d93ee7b533a8a829ccfb299f5b
yhh1973 <yhh1973@gmail.com>
2021-04-28 09:55:55 +0900

    비밀

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://yang:!mryang0928!@yang.ovzod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected.....'))
 .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Start World!'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
